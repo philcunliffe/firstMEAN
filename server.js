019d64ac27198d85dc84362dd1477f467eb25803
phillcunliffe <phillip.cunliffe@gmail.com>
2015-02-07 21:30:16 -0800

    Updates and adding sftp config file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+	mongoose = require('mongoose'),
 	stylus = require('stylus'),
 	logger = require('morgan'),
 	bodyParser = require('body-parser');
@@ -7,6 +8,10 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path) {
+	return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
@@ -14,13 +19,32 @@ app.use(bodyParser());
 app.use(stylus.middleware(
 	{
 		src: __dirname + '/public',
-		compile:compile
+		compile: compile
 	}
 ));
 app.use(express.static(__dirname + '/public'));
 
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+	console.log('multivision db opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+	mongoMessage =  messageDoc.message;
+})
+
+app.get('/partials/:partialPath', function (req, res) {
+	res.render('partials/' + req.params.partialPath)
+});
+
 app.get('*', function(req, res) {
-	res.render('index');
+	res.render('index', {
+		mongoMessage: mongoMessage
+	});
 });
 
 var port = 8000;
