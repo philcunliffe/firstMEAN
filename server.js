bd391109f278657255d1b8c8f6b7f6eba5c861cb
phillcunliffe <phillip.cunliffe@gmail.com>
2015-02-11 23:54:54 -0800

    Merge branch 'master' of https://github.com/phillcunliffe/firstMEAN
    Conflicts:
    server.js

--- server.js
+++ server.js
@@ -24,27 +24,40 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/multivision');
+var mongooseOptions = {
+	user: 'multiAdmin',
+	pass: 'password'
+};
+
+mongoose.connect('mongodb://localhost/multivision', mongooseOptions);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
 	console.log('multivision db opened');
 });
+<<<<<<< HEAD
+
+app.get('/partials/*', function (req, res) {
+	res.render('../../public/app/' + req.params[0]);
+=======
 var messageSchema = mongoose.Schema({message: String});
 var Message = mongoose.model('Message', messageSchema);
 var mongoMessage;
 Message.findOne().exec(function(err, messageDoc) {
-	mongoMessage =  messageDoc.message;
-})
+	if (err) {
+		console.log(err.toString());
+	} else {
+		mongoMessage =  messageDoc.message;	
+	}
+});
 
 app.get('/partials/:partialPath', function (req, res) {
-	res.render('partials/' + req.params.partialPath)
+	res.render('partials/' + req.params.partialPath);
+>>>>>>> FETCH_HEAD
 });
 
 app.get('*', function(req, res) {
-	res.render('index', {
-		mongoMessage: mongoMessage
-	});
+	res.render('index');
 });
 
 var port = 8000;
